5754ca52212d76d9ca7806838b7d6754399667bc
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-16 20:17:15 -0300

    Create TaskRvAdapter and start implementing TaskViewHolder inner class

---
+++ app/src/main/java/com/example/personaltasks/adapter/TaskRvAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.personaltasks.adapter
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.R
+import com.example.personaltasks.databinding.TileTaskBinding
+import com.example.personaltasks.model.Task
+import com.example.personaltasks.ui.OnTaskClickListener
+
+class TaskRvAdapter(
+    private val taskList: MutableList<Task>,
+    private val onTaskClickListener: OnTaskClickListener
+): RecyclerView.Adapter<TaskRvAdapter.TaskViewHolder>() {
+    inner class TaskViewHolder(ttb: TileTaskBinding): RecyclerView.ViewHolder(ttb.root) {
+        val titleTv: TextView = ttb.titleTv
+        val descriptionTv: TextView = ttb.descriptionTv
+        val dateTv: TextView = ttb.dateTv
+
+        init {
+            ttb.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
+                (onTaskClickListener as AppCompatActivity).menuInflater.inflate(R.menu.context_menu_main, menu)
+                menu.findItem(R.id.edit_task_mi).setOnMenuItemClickListener {
+                    onTaskClickListener.onEditTaskMenuItemClick(adapterPosition)
+                    true
+                }
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskRvAdapter.TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskRvAdapter.TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
