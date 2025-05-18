9045381b71dfa6f346b923dcab79f10e19579590
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-18 20:49:12 -0300

    feat: add MainBinding, Data source, adapter, controller and override functions

--- app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.example.personaltasks.ui
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.personaltasks.R
+import com.example.personaltasks.adapter.TaskRvAdapter
+import com.example.personaltasks.controller.MainController
+import com.example.personaltasks.databinding.ActivityMainBinding
+import com.example.personaltasks.model.Constant.EXTRA_TASK
+import com.example.personaltasks.model.Task
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnTaskClickListener {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //Data Source
+    private val taskList: MutableList<Task> = mutableListOf()
+
+    //Adapter
+    private val taskAdapter: TaskRvAdapter by lazy {
+        TaskRvAdapter(taskList, this)
+    }
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
+    //Controller
+    private val mainController: MainController by lazy {
+        MainController(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(amb.root)
+    }
+
+    override fun onTaskClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRemoveTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEditTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
     }
 }
