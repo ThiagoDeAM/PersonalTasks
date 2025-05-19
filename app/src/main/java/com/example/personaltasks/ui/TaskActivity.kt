63b040586a82fdaaccbf65738d06f33e1b7fd53c
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-18 21:37:19 -0300

    feat: inflate Activity

--- app/src/main/java/com/example/personaltasks/ui/TaskActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/TaskActivity.kt
@@ -1,4 +1,10 @@
 package com.example.personaltasks.ui
 
-class TaskActivity {
+import androidx.appcompat.app.AppCompatActivity
+import com.example.personaltasks.databinding.ActivityTaskBinding
+
+class TaskActivity : AppCompatActivity() {
+    private val atb: ActivityTaskBinding by lazy {
+        ActivityTaskBinding.inflate(layoutInflater)
+    }
 }
