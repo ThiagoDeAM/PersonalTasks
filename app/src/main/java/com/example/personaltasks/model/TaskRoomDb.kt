8c7951e099993e836e0bddc3eedd94b6975dd8f7
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-21 21:36:18 -0300

    style: add comments in TaskRoomDb

--- app/src/main/java/com/example/personaltasks/model/TaskRoomDb.kt
+++ app/src/main/java/com/example/personaltasks/model/TaskRoomDb.kt
@@ -4,8 +4,19 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
+/**
+ * Classe abstrata que representa o banco de dados local da aplicação.
+ * É a implementação do RoomDatabase que fornece acesso ao DAO.
+ * Define as entidades persistidas
+ * Define a versão do banco
+ */
 @Database(entities = [Task::class], version = 1)
-@TypeConverters(Converters::class)
+@TypeConverters(Converters::class) // Usado para tipos não primitivos
 abstract class TaskRoomDb: RoomDatabase() {
+
+    /**
+     * Retorna a instância de TaskDao.
+     * O Room gera automaticamente a implementação.
+     */
     abstract fun taskDao():TaskDao
 }
