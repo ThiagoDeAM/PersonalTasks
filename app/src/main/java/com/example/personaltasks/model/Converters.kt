a3d9dfc0c0878a0009e1f90c09aacb6a7daaa04b
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-21 21:24:18 -0300

    style: add comments in Converters

--- app/src/main/java/com/example/personaltasks/model/Converters.kt
+++ app/src/main/java/com/example/personaltasks/model/Converters.kt
@@ -4,9 +4,11 @@ import androidx.room.TypeConverter
 import java.util.Date
 
 class Converters {
+    // Converte um valor Long (timestamp) para um objeto Date.
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? = value?.let { Date(it) }
 
+    // Converte um objeto Date para um Long (timestamp).
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? = date?.time
 }
