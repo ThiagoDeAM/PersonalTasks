4ab6693a2a2346c70be36870de04421eccc6a2df
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-22 19:09:50 -0300

    style: add comments in OnTaskClickListener interface

--- app/src/main/java/com/example/personaltasks/ui/OnTaskClickListener.kt
+++ app/src/main/java/com/example/personaltasks/ui/OnTaskClickListener.kt
@@ -1,8 +1,15 @@
 package com.example.personaltasks.ui
 
+/**
+ * Interface responsável por definir os comportamentos de clique em tarefas.
+ */
 sealed interface OnTaskClickListener {
+    // Chamado quando o usuário clica em um item da lista de tarefas.
     fun onTaskClick(position: Int)
 
+    // Chamado quando o usuário seleciona a opção "Remover" no menu de contexto.
     fun onRemoveTaskMenuItemClick(position: Int)
+
+    // Chamado quando o usuário seleciona a opção "Editar" no menu de contexto.
     fun onEditTaskMenuItemClick(position: Int)
 }
