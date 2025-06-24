8891ff6e0adde6740aefa8142be7dd2e957c2d90
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-24 13:51:17 -0300

    feat: inflate activity

--- app/src/main/java/com/example/personaltasks/ui/DeletedTasksActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/DeletedTasksActivity.kt
@@ -6,16 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.personaltasks.R
+import com.example.personaltasks.databinding.ActivityDeletedTasksBinding
+import com.example.personaltasks.model.Task
 
 class DeletedTasksActivity : AppCompatActivity() {
+
+    private val adtb by lazy {
+        ActivityDeletedTasksBinding.inflate(layoutInflater)
+    }
+    private val deletedTaskList: MutableList<Task> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_deleted_tasks)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(adtb.root)
+
+        setSupportActionBar(adtb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Tarefas Excluídas"
+
+
     }
 }
