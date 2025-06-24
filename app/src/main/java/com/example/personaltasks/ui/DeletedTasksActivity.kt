1b99dbe252c82c7f5d2a35f97d8d0f5e5932b8f7
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-24 16:18:37 -0300

    feat: add instances and implement onCreate in DeletedTasksActivity

--- app/src/main/java/com/example/personaltasks/ui/DeletedTasksActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/DeletedTasksActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.personaltasks.R
+import com.example.personaltasks.adapter.DeletedTaskAdapter
+import com.example.personaltasks.controller.DeletedTasksController
 import com.example.personaltasks.databinding.ActivityDeletedTasksBinding
 import com.example.personaltasks.model.Task
 
@@ -14,7 +18,16 @@ class DeletedTasksActivity : AppCompatActivity() {
     private val adtb by lazy {
         ActivityDeletedTasksBinding.inflate(layoutInflater)
     }
+
     private val deletedTaskList: MutableList<Task> = mutableListOf()
+
+    private val deletedController by lazy {
+        DeletedTasksController(this)
+    }
+
+    private val deletedAdapter by lazy {
+        DeletedTaskAdapter(this, deletedTaskList, deletedController, lifecycleScope)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +35,10 @@ class DeletedTasksActivity : AppCompatActivity() {
 
         setSupportActionBar(adtb.toolbarIn.toolbar)
         supportActionBar?.subtitle = "Tarefas Excluídas"
+
+        adtb.deletedRv.adapter = deletedAdapter
+        adtb.deletedRv.layoutManager = LinearLayoutManager(this)
+
 
 
     }
