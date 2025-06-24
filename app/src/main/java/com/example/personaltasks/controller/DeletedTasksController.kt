933ea024116fec35393911c67e0549f00888fb3b
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-24 15:59:54 -0300

    feat: add DAO instance

--- app/src/main/java/com/example/personaltasks/controller/DeletedTasksController.kt
+++ app/src/main/java/com/example/personaltasks/controller/DeletedTasksController.kt
@@ -1,4 +1,16 @@
 package com.example.personaltasks.controller
 
-class DeletedTasksController {
+import android.content.Context
+import androidx.room.Room
+import com.example.personaltasks.model.TaskDao
+import com.example.personaltasks.model.TaskRoomDb
+
+class DeletedTasksController(context: Context) {
+
+    private val taskDao: TaskDao = Room.databaseBuilder(
+        context,
+        TaskRoomDb::class.java,
+        "task-database"
+    ).build().taskDao()
+
 }
