7fde18ca62f2243ac51f16193e00145156ee9cd9
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-25 16:08:34 -0300

    feat: create TaskFirebaseDatabase

---
+++ app/src/main/java/com/example/personaltasks/model/TaskFirebaseDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.personaltasks.model
+
+class TaskFirebaseDatabase: TaskDao {
+    override fun createTask(task: Task): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveTask(id: Int): Task {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveTasks(): MutableList<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTask(task: Task): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTask(task: Task): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDeletedTasks(): List<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveActiveTasks(): List<Task> {
+        TODO("Not yet implemented")
+    }
+
+}
