0fe782f319d6a2e9afc12de7522cbb973bdb0e11
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-26 17:05:34 -0300

    feat: implement clickListener to resetPassword button

--- app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
@@ -52,6 +52,15 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+
+        alb.resetPasswordBt.setOnClickListener {
+            signInCoroutineScope.launch {
+                val email = alb.emailLoginEt.text.toString()
+                if (email.isNotEmpty()) {
+                    Firebase.auth.sendPasswordResetEmail(email)
+                }
+            }
+        }
     }
 
     private fun openMainActivity() {
