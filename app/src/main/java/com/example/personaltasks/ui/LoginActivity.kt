5e447b47e142ca4dce52336a598bab6d233af440
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-26 16:34:36 -0300

    feat: add inflater

--- app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.personaltasks.R
+import com.example.personaltasks.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val alb: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(alb.root)
+
+        setSupportActionBar(alb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.login)
+
     }
 }
