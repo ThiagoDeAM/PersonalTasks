0a48a5f45f9212ecba21ddbdaead1055a940feba
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-26 17:03:03 -0300

    feat: implement clickListener to signIn button and add openMainActivity fun

--- app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
@@ -2,9 +2,15 @@ package com.example.personaltasks.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.personaltasks.R
 import com.example.personaltasks.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
         ActivityLoginBinding.inflate(layoutInflater)
     }
 
+    private val signInCoroutineScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +30,32 @@ class LoginActivity : AppCompatActivity() {
         alb.signUpBt.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+        alb.signInBt.setOnClickListener {
+            signInCoroutineScope.launch {
+                Firebase.auth.signInWithEmailAndPassword(
+                    alb.emailLoginEt.text.toString(),
+                    alb.passwordLoginEt.text.toString()
+                ).addOnFailureListener {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login failed. Cause: ${it.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }.addOnSuccessListener {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    openMainActivity()
+                }
+            }
+        }
+    }
+
+    private fun openMainActivity() {
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        finish()
     }
 }
